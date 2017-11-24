aaf815f4d2d761548c7052ae954c51848c5ed07d
Rodrigo Gil Lopetegui <grodrigo.dev@gmail.com>
2017-11-24 03:56:48 -0300

    added renew, CRUD author, and test for library app

--- catalog/models.py
+++ catalog/models.py
@@ -108,7 +108,10 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name"]
 
     def get_absolute_url(self):
         """
--- catalog/urls.py
+++ catalog/urls.py
@@ -8,10 +8,20 @@ urlpatterns = [
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 
     # Stubs are URL-friendly word-based primary keys for data. You might use a stub
     #if you wanted your book URL to be more informative.
     #For example /catalog/book/the-secret-garden rather than /catalog/book/33.
     #url(r'^book/(?P<stub>[-\w]+)$', views.BookDetailView.as_view(), name='book-detail'),
     #r'^book/(\d+)$' for unnamed argument
+]
+
+urlpatterns += [
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
